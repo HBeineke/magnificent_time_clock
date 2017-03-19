7154e4ecdf8e583069e6f53ae8854c8a620f53f7
Heiko Beineke <heiko.beineke@gmail.com>
2017-03-19 22:57:53 +0100

    Remove root reference

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :report, optional: true
-  belongs_to :root, class_name: "Report", optional: true
   has_many :reports
   accepts_nested_attributes_for :reports
   has_many :periods, as: :parent
